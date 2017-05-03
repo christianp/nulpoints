fd0616a0dd5a08fe81a45da09e370e780017c779
Christian Perfect <christianperfect@gmail.com>
2017-05-03 21:10:24 +0100

    show the leaderboard
    starting to make a tab switcher

--- thing.js
+++ thing.js
@@ -18,9 +18,6 @@ Vue.component('country', {
     props: {
         model: Country
     },
-    data: function() {
-        return {}
-    },
     computed: {
         points: function() {
             const i = this.model.index;
@@ -32,24 +29,85 @@ Vue.component('country', {
         }
     }
 });
+Vue.component('leaderboard-country', {
+    template: '#leaderboard-country-template',
+    props: {
+        model: Country
+    }
+});
+class Tab {
+    constructor(id,label) {
+        this.id = id;
+        this.label = label;
+    }
+}
+Vue.component('tab',{
+    template: '#tab',
+    props: ['model','currentTab'],
+    methods: {
+        setTab() {
+        }
+    }
+});
 
 const running_order = Object.keys(country_data);
 
-const all_countries = running_order.map(function(name,i){ return new Country(country_data[name],i+1)});
+const country_map = {}
+const all_countries = running_order.map(function(name,i){ 
+    const c = new Country(country_data[name],i+1);
+    country_map[name] = c;
+    return c;
+});
+
 let countries_unjudged, countries_judged, name='';
+const tabs = [new Tab('judging','Judge'), new Tab('leaderboard','Leaderboard')];
 let data = {
     countries_judged: [],
     countries_unjudged: all_countries.slice(),
-    name: ''
+    name: '',
+    leaderboard: {},
+    tab: tabs[0],
+    tabs: tabs
+
 }
 if('nulpoints' in localStorage) {
     let saved_data = JSON.parse(localStorage['nulpoints']);
-    data = {
-        countries_judged: saved_data.judged.map(x=>all_countries.find(c=>c.country==x)),
-        countries_unjudged: all_countries.filter(c=>saved_data.judged.indexOf(c.country)==-1),
-        name: saved_data.name
+    data.countries_judged = saved_data.judged.map(x=>all_countries.find(c=>c.country==x));
+    data.countries_judged.forEach(function(c,i){ c.index = i; c.judged = true })
+    data.countries_unjudged = all_countries.filter(c=>saved_data.judged.indexOf(c.country)==-1);
+    data.name = saved_data.name;
+}
+
+const RATE_URL = 'http://somethingorotherwhatever.com/nulpoints/rate.php';
+class Storage {
+    constructor(app) {
+        this.app = app;
+    }
+    save_local() {
+        let data = {
+            name: this.app.name,
+            judged: this.app.countries_judged.map(c=>c.country)
+        };
+        localStorage['nulpoints'] = JSON.stringify(data);
+    }
+    set_name(name) {
+        let d = new FormData();
+        d.append('command','set_name');
+        d.append('name',name);
+        fetch(RATE_URL,{method:'post',mode:'cors',credentials:'include',body:d})
+    }
+    set_ratings() {
+        const ratings = this.app.countries_judged.map(c=>c.country);
+        const d = new FormData();
+        d.append('command','set_ratings');
+        d.append('ratings',JSON.stringify(ratings));
+        fetch(RATE_URL,{method:'post',mode:'cors',credentials:'include',body:d})
+    }
+    get_leaderboard() {
+        return fetch(`${RATE_URL}?command=leaderboard`,{method:'GET',mode:'cors',credentials:'include'}).then(r=>r.json());
     }
 }
+
 
 const app = new Vue({
     el: '#app',
@@ -67,42 +125,26 @@ const app = new Vue({
             this.storage.set_ratings();
         },
         name: function() {
-            console.log('name',this.name);
             this.storage.save_local();
             this.storage.set_name(this.name);
         }
     },
     computed: {
+
+    },
+    mounted: function() {
+        const app = this;
+        app.storage = new Storage(app);
+        function load_leaderboard() {
+            app.storage.get_leaderboard().then(function(l) {
+                app.leaderboard = l.map(function(d) {
+                    const country = country_map[d.country];
+                    country.score = d.score;
+                    return country;
+                });
+            });
+        }
+        setInterval(load_leaderboard,5000);
+        load_leaderboard();
     }
 });
-
-const RATE_URL = 'http://somethingorotherwhatever.com/nulpoints/rate.php';
-class Storage {
-    constructor(app) {
-        this.app = app;
-    }
-    save_local() {
-        let data = {
-            name: this.app.name,
-            judged: this.app.countries_judged.map(c=>c.country)
-        };
-        console.log(data);
-        localStorage['nulpoints'] = JSON.stringify(data);
-    }
-    set_name(name) {
-        console.log('set name');
-        let d = new FormData();
-        d.append('command','set_name');
-        d.append('name',name);
-        fetch(RATE_URL,{method:'post',mode:'cors',credentials:'include',body:d})
-    }
-    set_ratings() {
-        const ratings = this.app.countries_judged.map(c=>c.country);
-        const d = new FormData();
-        d.append('command','set_ratings');
-        d.append('ratings',JSON.stringify(ratings));
-        fetch(RATE_URL,{method:'post',mode:'cors',credentials:'include',body:d})
-    }
-}
-
-app.storage = new Storage(app);
